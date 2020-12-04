5db88f31706ac476cfb3ddee348e2e87ec789652
Gijs <gijsvogels@hotmail.nl>
2020-12-04 20:55:59 +0100

    setup

---
+++ src/main/java/com/media/service/copic/Controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.media.service.copic.Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    String hello() {
+        return "Social Service";
+    }
+
+
+}
---
+++ src/main/java/com/media/service/copic/CopicApplication.java
@@ -0,0 +1,13 @@
+package com.media.service.copic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopicApplication.class, args);
+    }
+
+}
