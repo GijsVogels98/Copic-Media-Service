bf3d4a76b82b119d7b8b02a534883f871a1b6cd6
Gijs <gijsvogels@hotmail.nl>
2021-01-11 17:22:03 +0100

    cross

--- src/main/java/com/media/service/copic/Controllers/FireController.java
+++ src/main/java/com/media/service/copic/Controllers/FireController.java
@@ -4,14 +4,14 @@ package com.media.service.copic.Controllers;
 import com.media.service.copic.Service.FirebaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/media/")
 public class FireController {
 
     @Autowired
     private FirebaseService firebaseService;
-
+    @CrossOrigin("*")
     @PostMapping("/delete")
     public String delete(@RequestHeader String id){
         return firebaseService.deleteImg(id);
